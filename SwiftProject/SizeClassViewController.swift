b65ebac58f5ea039089dc68442181ac9c9c26b3e
魏柯岩 <270022748@qq.com>
2017-06-20 06:40:34 +0800

    add sizeClass

---
+++ SwiftProject/SizeClassViewController.swift
@@ -0,0 +1,38 @@
+
+//
+//  SizeClassViewController.swift
+//  SwiftProject
+//
+//  Created by 魏柯岩 on 17/6/20.
+//  Copyright © 2017年 SXJH. All rights reserved.
+//
+
+import UIKit
+
+class SizeClassViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override var shouldAutorotate: Bool{
+        return true
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
