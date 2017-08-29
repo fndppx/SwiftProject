1a4396eff8a161e91537d3255533d4ddfabac5d0
魏柯岩 <keyan.wei@qingclass.com>
2017-08-29 10:42:13 +0800

    hello

--- SwiftProject/AppDelegate.swift
+++ SwiftProject/AppDelegate.swift
@@ -12,7 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+//修改
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         self.window = UIWindow.init(frame: UIScreen.main.bounds)
